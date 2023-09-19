83371704382bda7d44d3d0e1bf03ba56d1ca6d60
Khoi Tran <tranquockhoi1999@gmail.com>
2023-09-19 19:25:26 +0700

    #

--- config/env.js
+++ config/env.js
@@ -5,5 +5,5 @@ export default {
     process.env.JWT_ACCESSTOKEN_PRIVATE_KEY || "accessTokenKhoitran",
   JWT_REFRESHTOKEN_PRIVATE_KEY:
     process.env.JWT_REFRESHTOKEN_PRIVATE_KEY || "refreshTokenKhoitran",
-  SERVER_URL: process.env.SERVER_URL || " 3.88.30.158:443",
+  SERVER_URL: process.env.SERVER_URL || " 54.81.143.84:443",
 };
